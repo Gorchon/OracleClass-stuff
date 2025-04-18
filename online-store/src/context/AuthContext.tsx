b8479a65dff97f3799f70a9ecae49d595f9a1976
Gorchon <a01254831@tec.mx>
2025-04-17 19:49:04 -0700

    Dev: New Component

---
+++ online-store/src/context/AuthContext.tsx
@@ -0,0 +1,58 @@
+// src/context/AuthContext.tsx
+import React, {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+  ReactNode,
+} from "react";
+import { User, onAuthStateChanged } from "firebase/auth";
+import { auth, getUserDisplayName, getUserRole } from "../services/api";
+
+interface AuthContextType {
+  user: User | null;
+  role: string;
+  loading: boolean;
+  displayName: string | null;
+}
+
+const AuthContext = createContext<AuthContextType>({
+  user: null,
+  role: "customer",
+  loading: true,
+  displayName: null,
+});
+
+export const AuthProvider: React.FC<{ children: ReactNode }> = ({
+  children,
+}) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [role, setRole] = useState<string>("customer");
+  const [displayName, setDisplayName] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      setUser(user);
+      if (user) {
+        const userRole = await getUserRole(user.uid);
+        setRole(userRole);
+        const name = await getUserDisplayName(user.uid);
+        setDisplayName(name);
+      } else {
+        setRole("customer");
+        setDisplayName(null);
+      }
+      setLoading(false);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, role, loading, displayName }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = (): AuthContextType => useContext(AuthContext);
